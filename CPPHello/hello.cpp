353dd50d819e882acc67b214346c57b96b6421ac
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-21 12:01:28 +0600

    install gcc,mingw,vscode & start codin on c++

---
+++ CPPHello/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    int a, b, sum;
+    a = 5;
+    b = 10;
+    sum = a + b;
+    cout << "total = " << sum;
+
+    return 0;
+}
